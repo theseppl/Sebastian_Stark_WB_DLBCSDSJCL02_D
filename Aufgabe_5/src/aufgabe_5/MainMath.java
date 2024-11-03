b9e88515a93325b42d6f60b6cc0d003cb31d9a78
Sebastian Stark <sstark@gmx.net>
2024-11-03 14:01:23 +0100

    Erster Commit Aufgabe 5

---
+++ Aufgabe_5/src/aufgabe_5/MainMath.java
@@ -0,0 +1,19 @@
+package aufgabe_5;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class MainMath {
+
+	public static void main(String[] args) {
+		BigDecimal dividend = new BigDecimal(23456);
+		BigDecimal divisor = new BigDecimal(10000);
+		
+		System.out.println("Division ohne Rundung: " 
+		+ dividend.divide(divisor));
+		
+		System.out.println("Division mit Rundung: " 
+				+ dividend.divide(divisor, 2, RoundingMode.HALF_UP));
+
+	}
+}
