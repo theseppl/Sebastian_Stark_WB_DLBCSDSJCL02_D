0b29f543c3245445c741a3262797afbf4be49689
Sebastian Stark <sstark@gmx.net>
2024-11-04 20:13:27 +0100

    Commit Klasse SwingMain

---
+++ Aufgabe_5/src/aufgabe_5/SwingMain.java
@@ -0,0 +1,16 @@
+ package aufgabe_5;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+public class SwingMain {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("GUI mit dem Paket java.swing"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		frame.setSize(300, 100); 
+		JCheckBox checkbox = new JCheckBox("Ja zum Fernstudium."); 
+		frame.getContentPane().add(checkbox);
+		frame.setVisible(true);
+	}
+}
